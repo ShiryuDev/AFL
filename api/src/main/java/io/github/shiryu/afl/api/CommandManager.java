65025dbc59d2f3448cbcb6cb588bbdb32f2474d7
ShiryuDev <efeendless@gmail.com>
2020-09-08 02:16:35 +0300

    Update CommandManager.java

--- api/src/main/java/io/github/shiryu/afl/api/CommandManager.java
+++ api/src/main/java/io/github/shiryu/afl/api/CommandManager.java
@@ -1,6 +1,36 @@
 package io.github.shiryu.afl.api;
 
-public abstract class CommandManager {
+import io.github.shiryu.afl.api.immutable.ImmutableCommand;
+import io.github.shiryu.afl.api.sender.CommandSender;
+import io.github.shiryu.afl.api.transformer.Transformer;
+import lombok.Getter;
+import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+@Getter
+public abstract class CommandManager<T> {
+
+    protected final List<ImmutableCommand> commands = new ArrayList<>();
+
+    protected final Map<Class<?>, Transformer> transformers = new HashMap<>();
+
+    public <P> void registerTransformer(@NotNull final Class<P> elementType, @NotNull final Transformer<P> transformer){
+        if (this.transformers.containsKey(elementType))
+            return;
+
+        this.transformers.put(elementType, transformer);
+    }
+
+    @NotNull
+    public Object transform(@NotNull final CommandSender sender, @NotNull final String value, @NotNull final Class<?> elementType){
+        return elementType.equals(String.class) ? value : this.transformers.get(elementType).transform(sender, value);
+    }
+
+    public abstract void registerCommand(@NotNull final CommandBase base);
+
+    public abstract void handle(@NotNull final T plugin);
 }
